d548bc42cb3c3940d2a51a7b7f64fb6c269551d5
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2023-06-23 16:33:16 -0300

    Create backend project and api application

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,9 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = 'http://127.0.0.1:8000/api/'
 
-get_response = requests.get(endpoint, data={"query": "Hello World"})
+get_response = requests.get(endpoint)
 print(get_response.json())
 print(get_response.status_code)
