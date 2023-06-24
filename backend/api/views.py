7ffc2d1ae917a0816417dd52787f0e55512d7296
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2023-06-24 13:50:42 -0300

    Echo GET Data - 47:11

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,26 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({'message': 'Hello, world!'})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    print(request)
+
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # strings of JSON - dict
+    except:
+        pass
+    print(data)
+    print(data.keys())
+
+    print(request.headers)
+    print(request.GET) # url query params
+
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    data['params'] = dict(request.GET)
+
+    return JsonResponse(data)
