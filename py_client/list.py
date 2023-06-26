0cccec6ba84213471ddd6eb93d2ef8f9e595f109
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2023-06-26 06:53:09 -0300

    Refactor ProductCreateAPIView into ProductListCreateAPIView - 1:44:36

---
+++ py_client/list.py
@@ -0,0 +1,14 @@
+import requests
+
+endpoint = 'http://localhost:8000/api/products/'
+
+data = {
+    'title': 'This field is done',
+    'price': 32.99,
+}
+
+get_response = requests.get(
+    endpoint,
+)
+
+print(get_response.text)
