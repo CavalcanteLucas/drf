ab2a47ecedf98b3f4de1e21124abfc0df2410352
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2023-06-26 10:11:45 -0300

    Create ProductsMixinView - 2:16:33

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('', views.ProductsMixinView.as_view(), name='product-list-create'),
     path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='product-update'),
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view(), name='product-delete'),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('<int:pk>/', views.ProductsMixinView.as_view(), name='product-detail'),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -45,6 +45,33 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
 
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
+
+
+class ProductsMixinView(
+        mixins.CreateModelMixin,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = "this is a nice signel view doing cool stuff"
+        serializer.save(content=content)
 
 
 @api_view(['GET', 'POST'])
